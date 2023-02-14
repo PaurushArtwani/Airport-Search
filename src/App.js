887fef878e0af87b02fd46e86a8574253241f16d
paurushbhai <55234702+paurushbhai@users.noreply.github.com>
2023-02-14 23:46:09 +0530

    All changes done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Maincontainer from "./components/Defaultconatiner";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Maincontainer />
+    </div>
+  );
+}
+
+export default App;
